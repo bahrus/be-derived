544d4f384d1a217692d65e65e6138d350d36d3e3
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-03-08 19:31:09 -0500

    Initial commit

--- be-derived.js
+++ be-derived.js
@@ -8,13 +8,56 @@ export class BeDerived extends EventTarget {
         survey = survey || affect;
         const { findRealm } = await import('trans-render/lib/findRealm.js');
         const realmToTransform = await findRealm(self, affect);
+        let affected = await findRealm(self, affect);
+        let split;
+        if (target !== undefined) {
+            const { beSplit } = await import('be-decorated/cpu.js');
+            split = await beSplit(target);
+        }
         console.log({ Derive });
+        const derivedVals = {};
         if (Derive !== undefined) {
             const { tryParse } = await import('be-decorated/cpu.js');
+            const realmToSurvey = await findRealm(self, survey);
+            if (!(realmToSurvey instanceof Element))
+                throw 'bD.404';
             for (const deriveStatement of Derive) {
                 const parsed = tryParse(deriveStatement, reDeriveMediumKey);
-                console.log({ deriveStatement, parsed });
+                if (parsed !== null) {
+                    const { camelQry, propName, propType } = parsed;
+                    const { getQuery } = await import('trans-render/lib/specialKeys.js');
+                    const queryInfo = getQuery(camelQry);
+                    const { query, attrib } = queryInfo;
+                    const srcElement = realmToSurvey.querySelector(query);
+                    console.log({ queryInfo });
+                    let val = undefined;
+                    if (attrib !== undefined) {
+                        val = srcElement.getAttribute(attrib);
+                    }
+                    else {
+                        if (srcElement instanceof HTMLAnchorElement) {
+                            val = srcElement.href;
+                        }
+                        else if (srcElement instanceof HTMLInputElement) {
+                            val = srcElement.value;
+                        }
+                        else {
+                            val = srcElement.textContent;
+                        }
+                    }
+                    switch (parsed.propType) {
+                        case 'number':
+                            val = Number(val);
+                            break;
+                        case 'date':
+                            val = new Date(val);
+                            break;
+                    }
+                    derivedVals[parsed.propName] = val;
+                }
+                //console.log({deriveStatement, parsed});
             }
+            console.log({ derivedVals });
         }
         if (realmToTransform === null)
             throw 'bD.404';
@@ -41,18 +84,14 @@ export class BeDerived extends EventTarget {
             await customElements.whenDefined('obj-ml');
             customElements.upgrade(resultDocument);
             const val = resultDocument.querySelector('obj-ml')?.value;
-            let affected = await findRealm(self, affect);
-            if (target !== undefined) {
-                const { beSplit } = await import('be-decorated/cpu.js');
-                const split = await beSplit(target);
-                if (split !== undefined) {
-                    const { setProp } = await import('trans-render/lib/setProp.js');
-                    await setProp(affected, split.path, val);
-                }
-            }
-            else {
-                Object.assign(affected, val);
-            }
+            Object.assign(derivedVals, val);
+        }
+        if (split !== undefined) {
+            const { setProp } = await import('trans-render/lib/setProp.js');
+            await setProp(affected, split.path, derivedVals);
+        }
+        else {
+            Object.assign(affected, derivedVals);
         }
         return mold;
     }
